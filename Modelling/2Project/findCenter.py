730e6cc62ecd10a74cb1fac46c9a6e7ba76a5f5f
theclough <tyler.clough@colorado.edu>
2024-10-13 12:22:54 -0600

    added tolerance check to findCenter()

--- Modelling/2Project/findCenter.py
+++ Modelling/2Project/findCenter.py
@@ -1,10 +1,11 @@
 import math
 
-def findCenter(Xs,Ys):
+def findCenter(Xs,Ys, tol):
 # finds center of circle defined by 3 *different* points
 # Inputs:
 #     -Xs : array of x values
 #     -Ys : array of y values
+#     -tol : tolerance check for 0
 # Outputs:
 #     (xC,yC) : tuple of coords for center
     x0,x1,x2 = Xs
@@ -15,12 +16,21 @@ def findCenter(Xs,Ys):
     y1 -= y0; y2 -= y0
     # subtract off (x0,y0)
     
-    xC = 0.5*(y1*(x2**2+y2**2)-y2*(x1**2+y1**2))/(x1*y2-x2*y1)
-    yC = 0.5*(x1*(x2**2+y2**2)-x2*(x1**2+y1**2))/(x2*y1-x1*y2)
+    xC = 0.5*(y1*(x2**2+y2**2)-y2*(x1**2+y1**2))/(x1*y2-x2*y1) - x0
+    yC = 0.5*(x1*(x2**2+y2**2)-x2*(x1**2+y1**2))/(x2*y1-x1*y2) - y0
     # use formula (derived by treating (x0,y0) = (0,0))
+    # - (x0,y0) to shift back into original coordinate system
 
-    return (xC-x0,yC-y0)
-    # put solution back in original coordinate system
+    if xC < tol:
+        if yC < tol:
+            xC,yC = (0,0)
+        else:
+            xC = 0
+    if yC < tol:
+        yC = 0
+    # check if xC and/or yC below tolerance
+        
+    return (xC,yC)
     
 
 def test():
@@ -29,9 +39,10 @@ def test():
     f = lambda y,x=1: math.cos(math.pi*x/y)
     g = lambda y,x=1: math.sin(math.pi*x/y)
     
-    Xs = [f(7),f(9),f(2)]
-    Ys = [g(7),g(9),g(2)]
+    Xs = [f(3,4),f(1),f(2,3)]
+    Ys = [g(3,4),g(1),g(2,3)]
+    tol = 10**(-10)
     
-    print(findCenter(Xs,Ys))
+    print(findCenter(Xs,Ys,tol))
 
 test()
